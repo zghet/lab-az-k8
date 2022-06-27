11a46eec08c5a1c196f0f86e3dfab3d35798375a
zghet <zghet@me.com>
2022-06-27 09:39:58 -0500

    Segundo Commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_resource_group" "k8sresourcegroup"{
+    name = var.resource_group_name
+    location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "k8cluster" {
+    name                    = var.cluster_name
+    location                = var.location
+    resource_group_name     = azure_resource_group.k8sresourcegroup.name
+    dns_prefix              = var.cluster_name
+
+    default_node_pool {
+        name                = "defaultk8s"
+        node_count          = var.system_node_count
+        vm_size             = "Standar_D2_v2"   
+    }
+
+    identity {
+        type = "SystemAssigned"
+    }
+
+    network_profile {
+        load_balancer_sku   = "standard"
+        network_plugin      = "kubenet"
+    }
+
+    tags = {
+        Enviroment = "Despliegue Parcial"
+    }
+}
+
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.k8cluster.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.k8cluster.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.k8cluster.node_resource_group
+}
+
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [azurerm_kubernetes_cluster.k8cluster]
+  filename     = "kubeconfig"
+  content      = azurerm_kubernetes_cluster.k8cluster.kube_config_raw
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "k8sresourcegroup"
+location            = "westus2"
+cluster_name        = "k8cluster"
+kubernetes_version  = "1.19.13"
+system_node_count   = 2
+acr_name            = "myacr3210"
